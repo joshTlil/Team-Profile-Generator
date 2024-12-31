54c1ba9bfa66d3c31b7cf69d66adbe36de67198b
JoshuaT <jttsat170@gmail.com>
2024-12-31 14:29:10 -0500

    Creating loop function still needs alot of work done

--- index.js
+++ index.js
@@ -18,13 +18,26 @@ const promptEmployee = ()=>{
             type: 'input',
             name: 'email',
             message: "What is your email"
+        },
+        {
+            type: 'confirm',
+            name: 'loop',
+            message: 'Add another employee?'
         }
     ])
+}
+
+const promptLoop = (data) =>{
+if(data.loop === true){
+promptEmployee()
+}
+console.log(data)
 }
 
 const init = () =>{
     promptEmployee()
     .then((data)=>{
+    promptLoop(data)
     writeFile("./dist/index.html", generateHtml(data), (err)=>{
         if (err) throw err;
         console.log("The file has been created")
