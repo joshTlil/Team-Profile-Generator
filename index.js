3a96673c0b5eef6d44731d6448f52244af2b5ae4
JoshuaT <jttsat170@gmail.com>
2025-01-01 07:49:15 -0500

    Understanding the project and its functions

--- index.js
+++ index.js
@@ -1,48 +1,28 @@
 import inquirer from 'inquirer';
 import { writeFile } from 'fs';
 import generateHtml from './dist/generateHtml.js';
+import Employee from './lib/employee.js';
 
-const promptEmployee = ()=>{
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is your name"
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Enter your id"
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "What is your email"
-        },
-        {
-            type: 'confirm',
-            name: 'loop',
-            message: 'Add another employee?'
-        }
-    ])
-}
-
-const promptLoop = (data) =>{
-if(data.loop === true){
-promptEmployee()
-}
-console.log(data)
-}
-
-const init = () =>{
-    promptEmployee()
-    .then((data)=>{
-    promptLoop(data)
-    writeFile("./dist/index.html", generateHtml(data), (err)=>{
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        message: 'What your id',
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+      },
+]).then(data=>{
+    const worker = new Employee(data.name, data.id, data.email)
+    writeFile("./dist/index.html", generateHtml(worker), (err)=>{
         if (err) throw err;
         console.log("The file has been created")
     })
-    })
-}
-
-init()
+})
