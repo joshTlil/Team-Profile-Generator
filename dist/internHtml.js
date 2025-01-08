83c271e3f5da7a667e993d1fa3934b0dfaeb5b4a
JoshuaT <jttsat170@gmail.com>
2025-01-08 01:29:40 -0500

    Finishing project for now

--- dist/internHtml.js
+++ dist/internHtml.js
@@ -1,8 +1,15 @@
 const internHtml = (intern) =>
-`<h1>${intern.name}</h1>
-<h1>${intern.id}</h1>
-<h1>${intern.email}</h1>
-<h1>${intern.school}</h1>
+`<div class="card" id="intern">
+<div class="cardHeader">
+<h1>${intern.name}</h1>
+<h1>Intern</h1>
+</div>
+<div class="info">
+<h1 class="id">${intern.id}</h1>
+<h1 class="email">${intern.email}</h1>
+<h1 class="school">${intern.school}</h1>
+</div>
+</div>
 ` 
 
 export default internHtml
